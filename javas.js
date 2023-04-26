d03dc639f7638e03144904093f1752d64da4ceca
YCN-AFS <70703361+YCN-AFS@users.noreply.github.com>
2023-04-26 20:02:00 +0700

    Chuc mung sinh nha

---
+++ javas.js
@@ -0,0 +1,11 @@
+<script>
+	var messages = ["Happy birthday to GFG",
+	"Happy birthday to GeeksforGeeks",
+	"Happy birthday to Geeks"];
+	
+	var i = messages.length;
+	var s = Math.floor(Math.random() * i);
+	
+	document.getElementById("msg")
+	.innerHTML = '" ' + messages[s] + ' "';
+</script>
